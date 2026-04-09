2e3a9da4b72409604991ed8e14a9ea5abd31f26f
pavankumar263 <gajulapallepavankumarreddy@gmail.com>
2026-04-09 16:00:41 +0530

    uc8

--- app/src/TrainConsistManagementApp.java
+++ app/src/TrainConsistManagementApp.java
@@ -1,39 +1,57 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-class Bogie {
-    String name;
-    int capacity;
+public class TrainConsistManagementApp {
 
 
-    Bogie(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
+    static class Bogie {
+        String name;
+        int capacity;
+
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+        public int getCapacity() {
+            return capacity;
+        }
+        public String toString() {
+            return name + " -> " + capacity;
+        }
+
     }
 
-
-    public String toString() {
-        return name + " -> " + capacity;
-    }
-}
-
-public class TrainConsistManagemantApp {
     public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("=== UC7 Sort Bogies by Capacity (Comparator) ===");
+        System.out.println("=================================\n");
 
 
         List<Bogie> bogies = new ArrayList<>();
 
 
         bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 54));
+        bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
+
+
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
 
 
         bogies.sort(Comparator.comparingInt(b -> b.capacity));
 
 
-        System.out.println("Bogies sorted by capacity (Ascending):");
+        System.out.println("\nAfter Sorting (by capacity):");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
+
+
+        System.out.println("\nUC7 sorting completed...");
     }
 }
